1f0289f475c1ebaa64d4e6fcad3b60380c919991
lwb <18614005205@163.com>
2019-11-28 00:17:46 +0800

    卡片呈现层级

--- app/src/main/java/com/xiangxue/rv_card_uidemo/SwipeCardLayoutManager.kt
+++ app/src/main/java/com/xiangxue/rv_card_uidemo/SwipeCardLayoutManager.kt
@@ -2,6 +2,7 @@ package com.xiangxue.rv_card_uidemo
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import java.util.logging.Level
 
 /**
  * Date:2019-11-26
@@ -20,18 +21,55 @@ class SwipeCardLayoutManager : RecyclerView.LayoutManager() {
         detachAndScrapAttachedViews(recycler!!)
 
         //8个
-        var bottomPosition = 0
+        var bottomPosition: Int
         // 0,1,2,3,4,5,6,7
         if (itemCount < CardConfig.MAX_SHOW_COUNT) {
             bottomPosition = 0
         } else {
-            bottomPosition = itemCount - CardConfig.MAX_SHOW_COUNT
+            bottomPosition = itemCount - CardConfig.MAX_SHOW_COUNT   // 8 - 4 = 4
         }
 
         for (i in bottomPosition until itemCount) {
             //复用
             val view = recycler.getViewForPosition(i)
+            addView(view)
 
+            measureChildWithMargins(view, 0, 0)
+
+            //求padding的总大小(剧中展示，算出边缘空白区域大小)
+            val widthSpace = width - getDecoratedMeasuredWidth(view)
+            val heightSpace = height - getDecoratedMeasuredHeight(view)
+
+            //布局
+            layoutDecoratedWithMargins(
+                view,
+                widthSpace / 2,
+                heightSpace / 2,
+                widthSpace / 2 + getDecoratedMeasuredWidth(view),
+                heightSpace / 2 + getDecoratedMeasuredHeight(view)
+            )
+
+            var level = itemCount - i - 1
+            // 8 - 4 -1 = 3
+            // 8 - 5 -1 = 2
+            // 8 - 6 -1 = 1
+            // 8 - 7 -1 = 0
+            if (level > 0) {
+                if (level < CardConfig.MAX_SHOW_COUNT - 1) {
+                    view.translationY = (CardConfig.TRANS_Y_GAP * level).toFloat()
+                    view.scaleX = 1 - CardConfig.SCALE_GAP * level
+                    view.scaleY = 1 - CardConfig.SCALE_GAP * level
+                } else {
+                    view.translationY = (CardConfig.TRANS_Y_GAP * (level - 1)).toFloat()
+                    view.scaleX = CardConfig.SCALE_GAP * (level - 1)
+                    view.scaleY = CardConfig.SCALE_GAP * (level - 1)
+                }
+            }
+//            if (level == 0) {
+//                view.translationY = (CardConfig.TRANS_Y_GAP * level).toFloat()
+//                view.scaleX = 1 - CardConfig.SCALE_GAP * level
+//                view.scaleY = 1 - CardConfig.SCALE_GAP * level
+//            }
         }
 
 
